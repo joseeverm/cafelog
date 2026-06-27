7df392dbba8e1012f27b6bb7742ef34e80eeac91
Jose Ever M <jmu3612@gmail.com>
2026-06-27 09:14:56 -0500

    fix: update base path in Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/cafelog-test/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
